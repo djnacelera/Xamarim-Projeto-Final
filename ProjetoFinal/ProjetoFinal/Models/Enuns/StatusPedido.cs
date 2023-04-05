94a11efb635d9e2550d2883862871e35dd7ae336
Joao Sambrano <joao.sambrano@outlook.com>
2023-04-05 19:36:12 -0300

    Primeiro_Commit

---
+++ ProjetoFinal/ProjetoFinal/Models/Enuns/StatusPedido.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoFinal.Model.Enuns
+{
+    public enum StatusPedido
+    {
+        Recebido = 1,
+        Preparando = 2,
+        Entregue = 3,
+        Cancelado = 4,
+        Baixado = 5,
+    }
+}
---
+++ ProjetoFinal/ProjetoFinal/Services/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjetoFinal.Services
+{
+    public interface IRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> ListarPedidosPreparando();
+
+        Task<string> AlterarPedidoParaEntregue(Guid id);
+    }
+}
